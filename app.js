cb47b4091aec388c89a28d7e5c539281ed872d30
David Sersch <dsersch640@gmail.com>
2018-07-19 11:57:20 -0700

    lesson 20

--- app.js
+++ app.js
@@ -16,9 +16,7 @@ if (command === "add") {
     var note = notes.addNote(argv.title, argv.body);
     if (note) {
         console.log('Note Created...');
-        console.log('--');
-        console.log(`Title: ${note.title}`);
-        console.log(`Body: ${note.body}`);
+        notes.logNote(note);
     } else {
         console.log("Title in use...");
     }
@@ -28,9 +26,7 @@ if (command === "add") {
     var note = notes.getNote(argv.title);
     if (note) {
         console.log('Note Found...');
-        console.log('--');
-        console.log(`Title: ${note.title}`);
-        console.log(`Body: ${note.body}`);
+        notes.logNote(note);
     } else {
         console.log("Title not found...");
     }
